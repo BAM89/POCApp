986c443a5f529327fe4fbfe5d11726ed5ada8f4d
Pettis Hartfield <pettish@frootfel.com>
2018-06-06 17:25:01 -0500

    Added Human Class
    Added Human Class

---
+++ app/src/main/java/com/frootfel/poc/Human.java
@@ -0,0 +1,24 @@
+package com.frootfel.poc;
+
+public class Human {
+
+    public String name;
+    public String direction;
+    private String larm;
+    private String rarm;
+    private String lleg;
+    private String rleg;
+
+    //Class Methods
+    public void Walk() {
+
+    }
+
+    public void Walk(int steps) {}
+
+    public void Talk(String words) { }
+
+    public void Talk() {}
+
+
+}
