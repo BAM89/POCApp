d74dba586952a00889d29690884c90845d21ea11
Bradley Monroe <monroe0219@gmail.com>
2018-06-06 20:07:33 -0500

    Added woman Subclass
    Added constructors

--- app/src/main/java/com/frootfel/poc/MainActivity.java
+++ app/src/main/java/com/frootfel/poc/MainActivity.java
@@ -2,18 +2,29 @@ package com.frootfel.poc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Human man;
+    private TextView tv;
+    private Woman female;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        man = new Human();
+        man.name = "Joe";
+        female = new Woman();
+        female.name ="Harmony";
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        tv = (TextView)findViewById(R.id.textviewName);
+        tv.setText(man.name + "  " + female.name );
 
     }
 }
---
+++ app/src/main/java/com/frootfel/poc/Woman.java
@@ -0,0 +1,5 @@
+package com.frootfel.poc;
+
+public class Woman extends Human {
+
+}
